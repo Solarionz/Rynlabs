16986c484e7f68d1e1e22fe051fce2455770056b
Solarion <97297099+Solarionz@users.noreply.github.com>
2023-04-04 21:37:55 +0200

    Update jailbreak.lua

--- jailbreak/jailbreak.lua
+++ jailbreak/jailbreak.lua
@@ -5,9 +5,11 @@ print("RynLabs Executed - Enjoy!")
 
 local SelfTab = Window:NewTab("Player");
 local TPs = Window:NewTab("Teleports");
+local Fight = Window:NewTab("Fight")
 
 local Section1 = SelfTab:NewSection("Player Settings");
 local Section2 = SelfTab:NewSection("Others");
+local Section3 = Fight:NewSection("Fight tools")
 
 Section1:NewLabel("Movement");
 Section2:NewLabel("Extra");
@@ -18,4 +20,13 @@ end)
 
 Section1:NewSlider("Run Speed", "Makes you faster", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+Section1:NewSlider("Jump Power", "Modify Your Jump Power", 500, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+end)
+
+
+Section3.NewButton("Killaura", "Enables kill aura", function()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Solarionz/Rynlabs/blob/main/jailbreak/killaura.lua"))()
 end)
