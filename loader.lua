71baba3e15741d058b13df22fc5a05d575c5167c
Solarion <97297099+Solarionz@users.noreply.github.com>
2023-04-04 20:19:09 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local games = {
-    [{155615604}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Jailbreak/Script.lua"
+    [{155615604}] = "https://github.com/Solarionz/Rynlabs/blob/main/jailbreak/jailbreak.lua"
 }
 
 for ids, url in next, games do
